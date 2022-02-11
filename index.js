b023ad5935bfb3ba7fc56067393eb4c341571ff2
manikanta sai <manikanta.sai@neutrinos.co>
2022-02-11 12:01:36 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+//mongodb connection
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.listen(8000, () => {
+  console.log("Backend is running");
+});
